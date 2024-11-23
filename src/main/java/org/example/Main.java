b1099fbfda6defa642f3b42a3eaa195a24228f97
Ekaterina Prokhorchuk <prokhorchuk.e@phystech.edu>
2024-11-23 13:25:09 +0300

    fix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,24 +1,6 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        WaitGroup wg = new WaitGroup();
-
-        FixedThreadPool fixedTP = new FixedThreadPool(104);
-        final int[] counter = { 0 };
-        for (int i = 0; i < 42; ++i) {
-            wg.add(1);
-            fixedTP.execute(() -> {
-                for (int j = 0; j < 10000; ++j) {
-                    synchronized (counter) {
-                        counter[0] += 1;
-                    }
-                }
-                wg.done();
-            });
-        }
-        fixedTP.start();
-        wg.await();
-        fixedTP.stop(false);
+    public static void main(String[] args) {
     }
 }
